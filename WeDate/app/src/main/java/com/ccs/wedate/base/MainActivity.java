9f6f2588581837d818106caaba1072fd29718ec8
Siriushe <1012108173@qq.com>
2016-05-06 01:13:42 +0800

    android struct

--- WeDate/app/src/main/java/com/ccs/wedate/base/MainActivity.java
+++ WeDate/app/src/main/java/com/ccs/wedate/base/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jason.wedate;
+package com.ccs.wedate.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ccs.wedate.R;
 
 public class MainActivity extends AppCompatActivity {
 
